4c9fa315c2c2b7eab1ad1e5d27ebbc5d6244497a
Kolozsi Márton <kolozsi.ocsi@gmail.com>
2024-12-05 16:20:07 +0100

    Initial commit

---
+++ src/main/java/hu/autoKereskedes/AutoKereskedes/AutoKereskedesApplication.java
@@ -0,0 +1,13 @@
+package hu.autoKereskedes.AutoKereskedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoKereskedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoKereskedesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/autoKereskedes/AutoKereskedes/AutoKereskedesApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.autoKereskedes.AutoKereskedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoKereskedesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
